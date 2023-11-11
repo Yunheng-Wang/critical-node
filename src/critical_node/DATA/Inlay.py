650aa73dc1a72a9a4de1301079d9cc14825f1fbf
Yunheng Wang <wangyunhenggxy@zjnu.edu.cn>
2023-11-11 10:54:24 +0800

    提交 critical-node 0.1.1 版本 (DataSet 添加)

--- src/critical_node/DATA/Inlay.py
+++ src/critical_node/DATA/Inlay.py
@@ -5,7 +5,7 @@ def All_Data_Set():
     :return: The collection of names of all data sets built into critical_node
     '''
     Existing_built_in_data_sets = [
-                                        'as','as19971108','bio-yeast-protein-inter','c_elegans','ca_astroph',
+                                        'as','as19971108','bio-yeast-protein-inter','beijing_road','c_elegans','ca_astroph',
                                         'ca_condmat','ca_hepph','ca_hepth','ca_qrqc','can2015',
                                         'dolphins','e_mail','email_enron','euroroad','facebook',
                                         'fb-pages-foods','foodweb-baydry','foodweb-baywet','hamster','jazz',
@@ -13,3 +13,6 @@ def All_Data_Set():
                                         'p2p-Gnutella08','p2p-Gnutella09','pgp','power_grid','soc-wiki-Votes','terrorist','us_air'
                                    ]
     return Existing_built_in_data_sets
+
+if __name__ == '__main__':
+    print(len(All_Data_Set()))
